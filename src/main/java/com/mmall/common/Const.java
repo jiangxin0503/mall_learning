110ffac4b36e5d760e2b1a7105297afb009e8c03
Jiang <I072973@PVGN50906800A.apj.global.corp.sap>
2017-05-08 16:53:19 +0800

    User Management

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,16 @@
+package com.mmall.common;
+
+/**
+ * Created by Xin on 5/4/2017.
+ */
+public class Const {
+
+    public static final String CURRENT_USER = "current_user";
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role{
+        int ROLE_CUSTOMER = 0;
+        int ROLE_ADMIN    = 1;
+    }
+}
